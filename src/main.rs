e4f0320c3e9b553ca7d2a66914effad44711113a
Aditya Dubey <hmmho705@gmail.com>
2026-09-18 23:29:37 +0530

    basic shii

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,26 @@
-fn main() {
-    println!("Hello, world!");
+use std::io;
+
+use crossterm::event::{self, Event, KeyCode, KeyEvent, KeyEventKind};
+use ratatui::{
+    buffer::Buffer,
+    layout::Rect,
+    style::Stylize,
+    symbols::border,
+    text::{Line, Text},
+    widgets::{Block, Paragraph, Widget},
+    DefaultTerminal, Frame
+};
+
+pub struct App{
+    counter: u8,
+    exit: bool
 }
+
+fn main() {
+    ratatui::run(|terminal| App::default().run(terminal))    
+}
+
+
+// impl App {
+//     pub fn run()
+// }
